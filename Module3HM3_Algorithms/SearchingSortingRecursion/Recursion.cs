36e0596ac3352f9c7e79cefda2a44b77b5d0ca98
MerCILLL <ted270503@gmail.com>
2023-09-30 17:37:14 +0300

    Added algorithms HM

---
+++ Module3HM3_Algorithms/SearchingSortingRecursion/Recursion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingSortingRecursion
+{
+    public static class Recursion
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}

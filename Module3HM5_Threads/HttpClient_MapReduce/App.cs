c01b6ce980a2310cd71b4e80766a2306f1cc10f9
MerCILLL <ted270503@gmail.com>
2023-10-08 16:48:50 +0300

    Memoization MapReduce Task

---
+++ Module3HM5_Threads/HttpClient_MapReduce/App.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClient_MapReduce
+{
+    public static class App
+    {
+		private static readonly HttpClient _client = new HttpClient();
+        private static readonly string _filesDirectory = "../../../Files";
+        
+        public static async Task Run()
+        {
+            await ReadWebsites();
+
+            var linesInFile1 = await ReadFileLineByLineAsync(GetFileName("site1.txt"));
+            var linesInFile2 = await ReadFileLineByLineAsync(GetFileName("site2.txt"));
+
+            var wordsInFile1 = Map(linesInFile1);
+            var wordsInFile2 = Map(linesInFile2);
+
+            var reducedResults = Reduce(wordsInFile1, wordsInFile2);
+
+            Console.WriteLine($"Total words in file 1: {GetTotalWords(wordsInFile1)}");
+            Console.WriteLine($"Total words in file 2: {GetTotalWords(wordsInFile2)}");
+
+            Console.WriteLine($"Unique words in file 1: {wordsInFile1.Count}");
+            Console.WriteLine($"Unique words in file 2: {wordsInFile2.Count}");
+
+            Console.WriteLine("Word counts in both files combined:");
+            foreach (var result in reducedResults)
+            {
+                Console.WriteLine($"Word: {result.Key}, Count: {result.Value}");
+            }
+
+
+        }
+
+        private static int GetTotalWords(Dictionary<string, int> wordCounts)
+        {
+            int total = 0;
+            foreach (var count in wordCounts.Values)
+                total += count;
+
+            return total;
+        }
+
+        private static async Task ReadWebsites()
+		{
+            string url1 = "https://learn.microsoft.com/en-us/azure/hdinsight/hadoop/apache-hadoop-dotnet-csharp-mapreduce-streaming";
+            string url2 = "https://dotnettutorials.net/lesson/map-reduce-framework/";
+
+            Task<string> taskSite1 = ReadWebsite(url1);
+            Task<string> taskSite2 = ReadWebsite(url2);
+
+            await Task.WhenAll(taskSite1, taskSite2);
+
+            CheckDirectoryExisting();
+
+            string site1File = GetFileName("site1.txt");
+            string site2File = GetFileName("site2.txt");
+
+            await File.WriteAllTextAsync(site1File, taskSite1.Result);
+            await File.WriteAllTextAsync(site2File, taskSite2.Result);
+
+        }
+        private static async Task<string> ReadWebsite(string url)
+		{
+				HttpResponseMessage response = await _client.GetAsync(url);
+				response.EnsureSuccessStatusCode();
+				string responseBody = await response.Content.ReadAsStringAsync();
+				return responseBody;
+		}
+
+        private static async Task<List<string>> ReadFileLineByLineAsync(string filePath)
+        {
+            var lines = new List<string>();
+
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = await file.ReadLineAsync()) != null) lines.Add(line);
+            }
+            return lines;
+        }
+
+        private static Dictionary<string, int> Map(List<string> lines)
+        {
+            var wordCounts = new Dictionary<string, int>();
+
+            foreach (var line in lines)
+            {
+                var words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in words)
+                {
+                    if (wordCounts.ContainsKey(word)) wordCounts[word]++;
+                    else wordCounts[word] = 1;
+                }
+            }
+
+            return wordCounts;
+        }
+
+        private static Dictionary<string,int> Reduce(Dictionary<string,int> wordsInFile1, Dictionary<string,int> wordsInFile2)
+        {
+            var bothFilesWords = new Dictionary<string, int>(wordsInFile1);
+
+            foreach (var word in wordsInFile2)
+            {
+                if(bothFilesWords.ContainsKey(word.Key)) bothFilesWords[word.Key] += word.Value;
+                else bothFilesWords[word.Key] = word.Value;
+            }
+
+            return bothFilesWords;
+        }
+
+        private static void CheckDirectoryExisting() => Directory.CreateDirectory(_filesDirectory);
+        private static string GetFileName(string fileTitle) => Path.Combine(_filesDirectory, fileTitle);
+
+	}
+}
---
+++ Module3HM5_Threads/MultyThreading/App.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultyThreading
+{
+    public static class App
+    {
+        private static ConcurrentDictionary<int, (int original, int fibonacci, int factorial)> dict = new();
+        public static async Task AppExecuteAsync()
+        {
+            Func<int, int> fibonacci = null;
+            Func<int, int> factorial = null;
+            fibonacci = Memoizer.Memoize((int n) => Fibonacci(n, fibonacci));
+            factorial = Memoizer.Memoize((int n) => Factorial(n, factorial));
+
+            Console.WriteLine("Enter numbers: ");
+            var input = Console.ReadLine();
+            var numbers = new List<int>();
+
+            foreach (var str in input.Split(' '))
+            {
+                if (int.TryParse(str, out var num))
+                {
+                    numbers.Add(num);
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                var fibTask = Task.Run(() => fibonacci(number));
+                var facTask = Task.Run(() => factorial(number));
+
+                await Task.WhenAll(fibTask, facTask);
+
+                dict.TryAdd(number, (number, fibTask.Result, facTask.Result));
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(
+                    $"Number: {item.Value.original}, " +
+                    $"Fibonacci: {item.Value.fibonacci}, " +
+                    $"Factorial: {item.Value.factorial}");
+            }
+        }
+
+        private static int Fibonacci(int n)
+        {
+            return Fibonacci(n, Fibonacci);
+        }
+
+        private static int Fibonacci(int n, Func<int, int> fibonacci)
+        {
+            if (n <= 2)
+                return 1;
+
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+        private static int Factorial(int n)
+        {
+            return Factorial(n, Factorial);
+        }
+
+        private static int Factorial(int n, Func<int, int> factorial)
+        {
+            if (n < 2)
+                return 1;
+
+            return n * Factorial(n - 1);
+        }
+    }
+}

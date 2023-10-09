b839fa7918a90a42fe28a5ed0c19ab91dee3b007
MerCILLL <ted270503@gmail.com>
2023-10-09 23:08:34 +0300

    Created model contact.cs, added getters setters validations

---
+++ Module3HM6_ContactListApp/Module3HM6_ContactListApp/Models/CountryCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HM6_ContactListApp.Models
+{
+    public enum CountryCode
+    {
+        Usa = 1,
+        Ukraine = 380
+    }
+}
---
+++ Module3HM6_ContactListApp/Module3HM6_ContactListApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Module3HM6_ContactListApp.Models;
+
+var contact1 = new Contact
+{
+    Name = "John",
+    Surname = "Doe",
+    Phone = "+380501112233",
+    Email = "john.doe@example.com"
+};
+
+var contact2 = new Contact
+{
+    Name = "Jane",
+    Surname = "Doe",
+    Phone = "+0504028750",
+    Email = "jane.doe@example.net"
+};
+
+var contact3 = new Contact
+{
+    Name = "Test",
+    Surname = "User",
+    Phone = "+1222334455",
+    Email = "test.user@example.com"
+};
+
+var contact4 = new Contact()
+{
+    Name = "qqq",
+    Surname = "qqq",
+    Phone = "+1222334455",
+    Email = "qqq@gmail.com"
+};
+
+//Console.WriteLine(contact1);
+//Console.WriteLine(contact2);
+//Console.WriteLine(contact3);
+Console.WriteLine(contact4);
